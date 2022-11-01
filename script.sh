7ccb416f271bf263634f3f1070b8e0a45bfd7677
Marina J Villalta Cerezo <marinavcsg@gmail.com>
2022-11-01 19:12:40 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo su
 cd
+cd Downloads 
 
-#.zshrc
-mkdir temp_zshrc
-cp ~/.zshrc temp_zshrc
-rm .zshrc
-wget https://github.com/KeepHowling/NewKaliVMsetup/blob/main/.zshrc
-wget https://raw.githubusercontent.com/KeepHowling/NewKaliVMsetup/main/.p10k.zsh
-zsh
+#pimpMyKali without input --> gets root login
+wget https://raw.githubusercontent.com/KeepHowling/NewKaliVMsetup/main/pimpMyKali_VnoInput.sh 
+chmod +x pimpMyKali_VnoInput.sh
+./pimpMyKali_VnoInput.sh
 
-mkdir freshInstall
-cd freshInstall
+#fonts --> set them in terminal
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
+cp Hack.zip /usr/share/fonts
+cd /usr/share/fonts; unzip Hack.zip
 
 #ls & cat
 wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd64.deb
@@ -21,13 +21,8 @@ mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
 #docker
-sudo apt install -y docker.io
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-#pimpMyKali without input
-wget https://raw.githubusercontent.com/KeepHowling/NewKaliVMsetup/main/pimpMyKali_VnoInput.sh 
-chmod +x pimpMyKali_VnoInput.sh
-./pimpMyKali_VnoInput.sh
+apt install -y docker.io
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 #classic tools oriented to bugbounty
 git clone https://github.com/JoyGhoshs/0install.git
@@ -35,6 +30,13 @@ cd 0install
 chmod +x 0install
 ./0install -i all
 
+#.zshrc
+mkdir temp_zshrc
+cp ~/.zshrc temp_zshrc
+rm .zshrc
+wget https://github.com/KeepHowling/NewKaliVMsetup/blob/main/.zshrc
+wget https://raw.githubusercontent.com/KeepHowling/NewKaliVMsetup/main/.p10k.zsh
+zsh
 
 #volatility2 y 3
 sudo apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
@@ -51,3 +53,12 @@ python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipyt
 python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
 echo 'export PATH=/root/.local/bin:$PATH' >> ~/.zshrc
 . ~/.zshrc
+
+
+#burpsuite cracked
+apt install libpcrecpp0v5 libc-ares-dev zlib1g-dev libuv1 libssl-dev libsodium-dev readline-common sqlite3 curl autoconf libtool g++ libcrypto++-dev libz-dev libsqlite3-dev libssl-dev libcurl4-gnutls-dev libreadline-dev libsodium-dev libc-ares-dev libfreeimage-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libmediainfo-dev libzen-dev
+apt install --reinstall build-essential
+mega-get 'https://mega.nz/folder/8d9FWBrT#LBMcN1WA-lJk6Bq7Bq023Q'
+#setup the license for your user with the v2022.3.6
+java -jar BurpLoaderKeygen.jar 
+#click and download the lastest burpsuite edition and copy the command on the loader
