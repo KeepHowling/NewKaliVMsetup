736f552ca8cc61c1f8657b5c3103fc09d0dc5a3e
Marina J Villalta Cerezo <marinavcsg@gmail.com>
2023-08-28 10:36:57 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,6 +9,12 @@ cd pimpmykali
 ./pimpmykali.sh
 N
 
+#sublimetext
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 #fonts --> set them in terminal
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
 cp Hack.zip /usr/share/fonts
